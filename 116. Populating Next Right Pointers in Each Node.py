8282cbab9db92a255620c830627266790fdeed66
NghiaDangTran <33323750+NghiaDangTran@users.noreply.github.com>
2022-04-03 22:51:43 -0500

    Add files via upload

---
+++ 116. Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,15 @@
+class Solution:
+    def connect(self, root):
+        if not root:
+            return
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            if curr.left and curr.right:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                stack.append(curr.right)
+                stack.append(curr.left)
+
+        return root
