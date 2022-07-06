e12f0ad4bc04d4c0dd17cc57f7bab20aa8502560
NghiaDangTran <33323750+NghiaDangTran@users.noreply.github.com>
2022-07-05 23:47:47 -0700

    Update 39. Combination Sum.py

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -1,21 +1,28 @@
 class Solution:
-    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
         result = []
+        if sum(candidates)<target:
+            return  []
+        if sum(candidates)==target:
+            return [candidates]
         
-
+       
+               
 
         def rec(data,currTarget,path):
-                     
+            
             if currTarget==0:
                 result.append(path)
                 return
 
-            if currTarget<0:
+            if currTarget<0 :
                 return
                 
             for i in range(len(data)):
-                
-                rec(data[i:],currTarget-data[i],path+[data[i]])
+                if i>0 and data[i]==data[i-1]:
+                    continue
+                rec(data[i+1:],currTarget-data[i],path+[data[i]])
 
-        rec(nums,target,[])
+        rec(candidates,target,[])
         return result
